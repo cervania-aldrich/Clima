0837a08683447123ed8d2c956f8bef6494638386
Aldrich Cervania <aldrich@cervania.com>
2023-04-23 13:57:45 +0100

    Dark mode and working with vector assets

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -1,16 +1,8 @@
-//
-//  ViewController.swift
-//  Clima
-//
-//  Created by Angela Yu on 01/09/2019.
-//  Copyright © 2019 App Brewery. All rights reserved.
-//
-
 import UIKit
 
 class WeatherViewController: UIViewController {
 
-    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var conditionImageView: UIImageView! //Images come from SF Symbols
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
     
