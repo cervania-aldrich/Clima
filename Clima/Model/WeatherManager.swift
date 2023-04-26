9aa9a76c2cad93d37a1c758486f56cadd0105a68
Aldrich Cervania <aldrich@cervania.com>
2023-04-26 23:47:05 +0100

    Add URLSession for networking

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -10,14 +10,54 @@ struct WeatherManager {
         
         //URLComponents already contains the proper percent encoding. Using this object results in more safe code for networking.
         
-        guard var urlString = URLComponents(string: weatherURL) else { return } //Create URL
-        urlString.scheme = "https"
-        
-        urlString.queryItems = [URLQueryItem(name: "q", value: cityName),
+        guard var urlComponent = URLComponents(string: weatherURL) else { return } //Create URL
+        urlComponent.scheme = "https"
+        urlComponent.queryItems = [URLQueryItem(name: "q", value: cityName),
                                 URLQueryItem(name: "appid", value: apiKey),
                                 URLQueryItem(name: "units", value: unitOfMeasurement)] //Create queries, and add them to the URL.
         
-        //print(urlString.url!.absoluteString) //Test I'm getting the corrent string
+        let urlString = urlComponent.url!.absoluteString
+        
+        sendRequest(urlString)
+        
+    }
+    
+    /**
+     The function that performs the networking.
+    
+     Networking involves 4 steps:
+     1. Create a URL object.
+     2. Create a URLSession.
+     3. Create a task for the URLSession.
+     4. Start the task.
+    */
+    func sendRequest(_ urlString: String) {
+        
+        //Create a URL object
+        guard let url = URL(string: urlString) else { return }
+        
+        //Create a URLSession.
+        let session = URLSession(configuration: .default)
+        
+        //Create a task for the URLSession.
+        let task = session.dataTask(with: url) { data, response, error in
+            
+            if error != nil {
+                print(error!)
+                return
+            }
+            
+            if let safeData = data {
+                let dataString = String(data: safeData, encoding: .utf8)
+                print(dataString!)
+            }
+            
+            
+            
+        }
+        
+        //Start the task.
+        task.resume()
         
     }
     
