ac83288d4f99d1e1f0d1dd9576ad401c47a596e3
Aldrich Cervania <aldrich@cervania.com>
2023-05-04 01:09:15 +0100

    Add extensions to refactor WeatherViewController

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -1,7 +1,7 @@
 import UIKit
 
-class WeatherViewController: UIViewController, UITextFieldDelegate, WeatherManagerDelegate {
-
+class WeatherViewController: UIViewController {
+    
     @IBOutlet weak var conditionImageView: UIImageView! //Images come from SF Symbols
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
@@ -25,16 +25,22 @@ class WeatherViewController: UIViewController, UITextFieldDelegate, WeatherManag
         view.endEditing(true)
     }
     
+}
+
+// MARK: - UITextFieldDelegate
+
+extension WeatherViewController: UITextFieldDelegate {
+    
     ///A function that defines the behaviour when the search button is pressed. That desired behaviour is to search for the weather based on the users input.
     @IBAction func searchPressed(_ sender: UIButton) {
         searchTextField.endEditing(true) //Dismiss the keyboard
     }
     
     /**
-    A function that defines the behaviour when the user has pressed the return key on the iPhone keyboard (Pressing enter on the keyboard).
+     A function that defines the behaviour when the user has pressed the return key on the iPhone keyboard (Pressing enter on the keyboard).
      
-    We return true, because we want to process the search key, and run this function. Which means, this behaviour is analogous to having an IBAction to that return button.
-    */
+     We return true, because we want to process the search key, and run this function. Which means, this behaviour is analogous to having an IBAction to that return button.
+     */
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         searchTextField.endEditing(true) //Dismiss the keyboard
         return true
@@ -51,15 +57,15 @@ class WeatherViewController: UIViewController, UITextFieldDelegate, WeatherManag
         searchTextField.text = "" //Clears the textField so we can enter a new search term.
     }
     /**
-    A function that defines the behaviour when the user tries to deselect the textField.
-    
-    This function is useful for validating what the user has typed, so we don't just trap the user in editing mode.
+     A function that defines the behaviour when the user tries to deselect the textField.
      
-    Note that this function is called by the UITextField class, not this ViewController (similar to the other textField delegate methods). Since we only have one textField in the app, then it is this textField (searchTextField) that calls this method. But we could have had more textField's triggering this method.
+     This function is useful for validating what the user has typed, so we don't just trap the user in editing mode.
      
-    This is analogous to linking multiple buttons to a single IBAction. Multiple textField's can be linked to the one textField class.
+     Note that this function is called by the UITextField class, not this ViewController (similar to the other textField delegate methods). Since we only have one textField in the app, then it is this textField (searchTextField) that calls this method. But we could have had more textField's triggering this method.
      
-    Like the sender parameter of an IBAction, the textField parameter can tell which textField is which (as long as those textField's sets its delegate to the current class: i.e. conforming to UITextFieldDelegate).
+     This is analogous to linking multiple buttons to a single IBAction. Multiple textField's can be linked to the one textField class.
+     
+     Like the sender parameter of an IBAction, the textField parameter can tell which textField is which (as long as those textField's sets its delegate to the current class: i.e. conforming to UITextFieldDelegate).
      */
     func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
         
@@ -73,14 +79,18 @@ class WeatherViewController: UIViewController, UITextFieldDelegate, WeatherManag
             return false //Keep the keyboard where it is until the user as entered a city name.
         }
     }
-    
+}
+
+// MARK: - WeatherManagerDelegate
+
+extension WeatherViewController: WeatherManagerDelegate {
     /**
-    This function comes from the WeatherManagerDelegate protocol, where in this conforming class we are providing the actual implemetation for the protocol requirements.
-    
-    We wrap the code that modifies the UI with an asynchronous dispatch call to the main thread, because currently the UI is being updated on the background and not the main thread where it's suppose to be due to the UI using information from a completion handler, where completion handlers are done in the background thread. XCode's main threader tool will also warn us of this error.
-    
-    - parameter weatherManager: The identity of the object that calls this delegate method. It is an Apple naming convention to include this first parameter in your delegate methods. This also makes any properties available to the method.
-    - parameter weather: The object that has the information (response) from openweather api as a Swift Object, (after decoding the JSON)
+     This function comes from the WeatherManagerDelegate protocol, where in this conforming class we are providing the actual implemetation for the protocol requirements.
+     
+     We wrap the code that modifies the UI with an asynchronous dispatch call to the main thread, because currently the UI is being updated on the background and not the main thread where it's suppose to be due to the UI using information from a completion handler, where completion handlers are done in the background thread. XCode's main threader tool will also warn us of this error.
+     
+     - parameter weatherManager: The identity of the object that calls this delegate method. It is an Apple naming convention to include this first parameter in your delegate methods. This also makes any properties available to the method.
+     - parameter weather: The object that has the information (response) from openweather api as a Swift Object, (after decoding the JSON)
      **/
     func didUpdateWeather(_ weatherManager:WeatherManager, _ weather: WeatherModel) {
         
@@ -90,13 +100,10 @@ class WeatherViewController: UIViewController, UITextFieldDelegate, WeatherManag
             self.conditionImageView.image = UIImage(systemName: weather.conditionName)
         }
         
-        
     }
     
     func didFailWithError(_ weatherManager: WeatherManager, _ error: Error) {
         print(error)
     }
-    
-    
 }
 
