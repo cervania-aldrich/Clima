bb9b07469b12ac2909cf4c1bed3b72a10b9cfd33
Aldrich Cervania <aldrich@cervania.com>
2023-05-10 17:45:03 +0100

    update apiKey

--- Clima/ConstantAPIKey.swift
+++ Clima/ConstantAPIKey.swift
@@ -1,5 +1,5 @@
 struct ConstantAPIKey {
     
-    static let apiKey = "a210b6f91c665d2db9eb566589c6c2aa" // Please insert your API key from OpenWeather API here!
+    static let apiKey = "Please_Enter_API_Key_From_OpenWeather_Here" // Please insert your API key from OpenWeather API here!
     
 }
