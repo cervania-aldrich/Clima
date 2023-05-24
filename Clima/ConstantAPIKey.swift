79ea05bc1125ef2ada6fd090f3dd58d375134403
Aldrich Cervania <aldrich@cervania.com>
2023-05-24 15:55:49 +0100

    give apiKey example as a comment

--- Clima/ConstantAPIKey.swift
+++ Clima/ConstantAPIKey.swift
@@ -1,5 +1,5 @@
 struct ConstantAPIKey {
     
-    static let apiKey = "Please_Enter_API_Key_From_OpenWeather_Here" // Please insert your API key from OpenWeather API here!
+    static let apiKey = "Please_Enter_API_Key_From_OpenWeather_Here" // For example: "g752a6q91r665t2dy1mb123987k6c2vc"
     
 }
